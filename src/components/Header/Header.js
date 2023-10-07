8a62391372a13dc09bce2a3c674b75d32162f0fa
Muhaimin Jobayer <mmj0993@gmail.com>
2023-10-07 04:11:12 -0500

    Implement menu icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,44 @@
+import { useState } from 'react';
 import './Header.css';
 
 const Header = () => {
-  return (
-    <header className="header">
-      {/* Header content */}
-    </header>
-  );
-}
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    // Function to toggle the navigation menu
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+    return (
+        <header className='header'>
+            <h1 className='heading'>
+                <a href='#home'>Muhaimin Jobayer</a>
+            </h1>
+            <div className='navigation'>
+                <div class='menu-icon' onClick={toggleNav}>
+                    <div className={`bar ${isNavOpen ? 'open' : ''}`}></div>
+                    <div className={`bar ${isNavOpen ? 'open' : ''}`}></div>
+                    <div className={`bar ${isNavOpen ? 'open' : ''}`}></div>
+                </div>
+
+                <nav className='nav'>
+                    <ul className='nav-list'>
+                        <li className='nav-item'>
+                            <a href='#about'>About</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a href='#skills'>Skills</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a href='#projects'>Projects</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a href='#contact'>Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
 
 export default Header;
