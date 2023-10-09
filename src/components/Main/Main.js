b7398f71dfba42e71def711149457bb623e4faaf
Muhaimin Jobayer <mmj0993@gmail.com>
2023-10-08 23:49:35 -0500

    Import and render components for main section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,18 @@
+import Intro from '../Intro/Intro';
+import About from '../About/About';
+import Skills from '../Skills/Skills';
+import Projects from '../Projects/Projects';
+import Contact from '../Contact/Contact';
 import './Main.css';
 
 const Main = () => {
   return (
     <main className="main">
-      {/* Main content */}
+      <Intro />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </main>
   );
 }
