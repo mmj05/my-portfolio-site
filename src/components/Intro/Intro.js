1f0236c4a85bb07206623fb65cae1eaf49aeac78
Muhaimin Jobayer <mmj0993@gmail.com>
2023-10-08 23:48:34 -0500

    Add profile pic and short bio

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,9 +1,18 @@
 import './Intro.css';
+import image from '../../assets/profile-3.jpg';
 
 const Intro = () => {
   return (
     <section className="intro">
-      {/* Your intro content */}
+      <div className='profile-pic'>
+        <img src={image} alt='Muhaimin' />
+      </div>
+      <div className='bio'>
+        <h2>Muhaimin Jobayer</h2>
+        <p>Crafting Digital Adventures 🚀</p>
+        <p>Code by Day, Dream by Night 🌙</p>
+        <p>Building the Web, One Byte at a Time 🎨</p>
+      </div>
     </section>
   );
 }
