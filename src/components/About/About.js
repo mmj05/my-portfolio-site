c863854b8aee109ea5e5b0acc41c48391624bd25
muhaiminjobayer <mmj0993@gmail.com>
2023-11-18 15:45:04 -0600

    Updated about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,27 +3,30 @@ import image from '../../assets/work-from-home.png';
 
 const About = () => {
     return (
-        <section id="about">
-            <h2 className="about-heading">About</h2>
-            <div className="flex-container">
-                <div className="about-container">
+        <section id='about'>
+            <h2 className='about-heading'>About</h2>
+            <div className='flex-container'>
+                <div className='about-container'>
                     <p>
-                        Greetings! I am Muhaimin, a software engineer
-                        specializing in the development of high-performance
-                        applications designed to address complex real-world
-                        challenges. With a versatile skill set spanning Full Stack, Front-End, and Back-End, I embrace continuous learning, thriving on demanding obstacles. 
-                    </p>
-                    <p>
-                        Throughout my career, I have diligently refined my
-                        expertise in various facets of web development and
-                        design. Currently, I am leveraging the power of React and Spring Boot while continually broadening my expertise with new tools and technologies.
+                        Greetings! I am Muhaimin, a full-stack software
+                        engineer with a focus on <span>React, TypeScript, Java, Spring
+                        Boot,</span> and proficient database management using
+                        <span> PostgreSQL and MySQL</span>. My passion lies in crafting
+                        seamless user interfaces and architecting robust backend
+                        systems, addressing the complexities of modern web
+                        development. Over the course of my career, I have
+                        continuously honed my skills in various aspects of web
+                        development and design. Currently, I am harnessing the
+                        capabilities of React and Spring Boot, all while
+                        expanding my proficiency with the integration of new
+                        tools and technologies.
                     </p>
                 </div>
-                <div className="pic-container">
+                <div className='pic-container'>
                     <img
                         src={image}
-                        alt="work-from-home"
-                        className="about-dp"
+                        alt='work-from-home'
+                        className='about-dp'
                     />
                 </div>
             </div>
